4beebf7cf68bc84566fb75322614785f34538525
TruongHuynh01981340 <TruongThinh_Huynh@student.uml.edu>
2025-04-16 19:52:50 -0400

    Everything our team haave done so far. Will not push to main yet since firebase implementation is not done.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styles/LandingPage.css";
+import petImage1 from "../assets/images/bird.jpg";
+import petImage2 from "../assets/images/fish.jpg";
+import petImage3 from "../assets/images/dog.jpg";
+import petImage4 from "../assets/images/catplaying.jpg";
+import petImage5 from "../assets/images/rabbit.jpg";
+import petImage6 from "../assets/images/kitty.jpg";
+import petImage7 from "../assets/images/guineapig.jpg"
+import petImage8 from "../assets/images/dogandcat.jpg";
+import petImage9 from "../assets/images/lizard.jpg";
+import petImage10 from "../assets/images/2dogs.jpg";
 
 const LandingPage = () => {
 
@@ -32,11 +42,11 @@ const LandingPage = () => {
 
         <div className="slider-container sliders" id="slider1">
             <div className="slider-track">
-                <div className="slide"><img src="src/assets/images/bird.jpg" alt="Bird" /></div>
-                <div className="slide"><img src="src/assets/images/fish.jpg" alt="Fish" /></div>
-                <div className="slide"><img src="src/assets/images/dog.jpg" alt="Dog" /></div>
-                <div className="slide"><img src="src/assets/images/catplaying.jpg" alt="Cat playing" /></div>
-                <div className="slide"><img src="src/assets/images/rabbit.jpg" alt="Rabbit" /></div>
+                <div className="slide"><img src={petImage1} alt="Bird" /></div>
+                <div className="slide"><img src={petImage2} alt="Fish" /></div>
+                <div className="slide"><img src={petImage3} alt="Dog" /></div>
+                <div className="slide"><img src={petImage4} alt="Cat playing" /></div>
+                <div className="slide"><img src={petImage5} alt="Rabbit" /></div>
             </div>
         </div>
 
@@ -62,11 +72,11 @@ const LandingPage = () => {
 
         <div className="slider-container sliders" id="slider2">
             <div className="slider-track">
-                <div className="slide"><img src="src/assets/images/kitty.jpg" alt="Kitty" /></div>
-                <div className="slide"><img src="src/assets/images/guineapig.jpg" alt="Guinea Pig" /></div>
-                <div className="slide"><img src="src/assets/images/dogandcat.jpg" alt="Dog and a cat" /></div>
-                <div className="slide"><img src="src/assets/images/lizard.jpg" alt="Lizard" /></div>
-                <div className="slide"><img src="src/assets/images/2dogs.jpg" alt="2 Dogs" /></div>
+                <div className="slide"><img src={petImage6} alt="Kitty" /></div>
+                <div className="slide"><img src={petImage7} alt="Guinea Pig" /></div>
+                <div className="slide"><img src={petImage8} alt="Dog and a cat" /></div>
+                <div className="slide"><img src={petImage9} alt="Lizard" /></div>
+                <div className="slide"><img src={petImage10} alt="2 Dogs" /></div>
             </div>
         </div>
 
