b5c3262c7d4337cee4602d82813ea3829d2ce4d8
TruongHuynh01981340 <TruongThinh_Huynh@student.uml.edu>
2025-04-29 21:06:24 -0400

    Implemented top bar and make reminder page responsive and mobile compatible

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,42 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../assets/styles/Navbar.css";
+import { getAuth, signOut } from "firebase/auth";
 
 function Navbar() {
   const location = useLocation();
+  const navigate = useNavigate();
 
-  const hideNavbarRoutes = ['/authlanding'];
+  const hideNavbarRoutes = ['/authlanding', '/'];
 
   if (hideNavbarRoutes.includes(location.pathname)) {
     return null;
   }
 
+  const handleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        localStorage.clear();
+
+        navigate("/authlanding");
+      })
+      .catch((error) => {
+        console.error("Navbar: Failed signing out", error);
+      })
+  }
+
   return (
     <nav className="navbar_container">
-      <div className="navbar_left"></div>
-      <Link id="navbar_link_logo" className="navbar_link_logo" to="/">PetRemind</Link>
+      <div className="navbar_left">
+        <Link id="navbar_link_logo" className="navbar_link_logo" to="/">PetRemind</Link>
+      </div>
       <div className="navbar_right">
         <Link id="navbar_link_home" className="navbar_link" to="/authlanding">Home</Link>
         <Link id="navbar_link_petprofile" className="navbar_link" to="/petprofile">Pet Profile</Link>
         <Link id="navbar_link_reminder" className="navbar_link" to="/reminder">Reminder</Link>
         <Link id="navbar_link_features" className="navbar_link" to="/features">Features</Link>
+        <span id="navbar_link_signout" className="navbar_link" onClick={handleSignOut}>Sign Out</span>
       </div>
     </nav>
   );
